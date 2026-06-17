34eab18ca88cd1b2ef533fb96ff181b62fee1cb7
Everson Cunha <82178162+CunhaEverson@users.noreply.github.com>
2026-06-17 09:11:58 -0300

    Atualizar o produtos.js

--- javascript/produtos.js
+++ javascript/produtos.js
@@ -5,7 +5,7 @@ const produtos = [
         title: 'Pão Recheado',
         description: 'Sabores: frango ou calabresa, com requijão e orégano.',
         value: 10.00,
-        estoque: 10
+        estoque: 0
     },
     {
         id: 2,
