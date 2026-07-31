d91c43f55f65a472fd0b80cf28e8ae4e8fd6d2b6
Everson Cunha <82178162+CunhaEverson@users.noreply.github.com>
2026-07-31 08:24:14 -0300

    Atualizar o produtos.js

--- javascript/produtos.js
+++ javascript/produtos.js
@@ -13,7 +13,7 @@ const produtos = [
         title: 'Empada',
         description: 'Recheadas nos sabores de frango e camarão, com creme de requeijão.\n A Empada de Camarão é R$: 10,00.',
         value: 8.00,
-        estoque: 10
+        estoque: 0
     },
     {
         id: 3,
